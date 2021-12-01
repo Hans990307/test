3323e4e0788e9ef2f2f01bd4b219ad68eac867ae
unknown <hans>
2021-12-01 10:52:57 +0800

    initial commit

---
+++ guru99/Guru99demo.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru99Demo
+{
+    class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\user\\Desktop\\chrome");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.google.com/?hl=ms";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
---
+++ guru99/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guru99
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
